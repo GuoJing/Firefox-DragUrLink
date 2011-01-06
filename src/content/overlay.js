b07f2acbf72270143ee80b430094178d1129f710
郭靖 <soundbbg@gmail.com>
2011-01-06 16:44:43 +0800

    add files

---
+++ src/content/overlay.js
@@ -0,0 +1,111 @@
+var DragUrLink =
+{
+    x:0,
+    y:0,
+    times:0,
+    method:"new",
+    rx:0,
+    ry:0,
+    pref:null,
+
+    OnMainLoad: function()
+    {
+        DragUrLink.pref = new ___.PrefImpl();
+
+        if(!DragUrLink.pref.getValue("Installed"))
+        {
+            DragUrLink.pref.setValue("Installed",true);
+            DragUrLink.pref.setValue("LinkOption",1);
+        }
+
+        getBrowser().setAttribute('ondragend', 'DragUrLink.GoToPage(event,\'no\')');
+        getBrowser().setAttribute('ondragstart', 'DragUrLink.StartDrag(event,\'no\')');
+        getBrowser().setAttribute('ondragover', 'DragUrLink.StartDrag(event,\'no\')');
+        getBrowser().setAttribute('ondrop', 'DragUrLink.StartDrag(event,\'no\')');
+        getBrowser().setAttribute('onmousedown', 'DragUrLink.StartDrag(event,\'down\')');
+        getBrowser().setAttribute('onmouseup', 'DragUrLink.StartDrag(event,\'up\')');
+    },
+
+    GoToPage:function(event)
+    {
+        var value = event.dataTransfer.getData("text/uri-list");
+        if(value != ""&&value != null)
+        {
+            this.times=0;
+            var option = DragUrLink.pref.getValue("LinkOption");
+
+            switch(option)
+            {
+                case 0:openUILink(value, null, false, true);break;
+                case 1:gBrowser.addTab(value);break;
+                case 2:gBrowser.selectedTab = gBrowser.addTab(value);break;
+            }
+        }
+    },
+
+    GoTo:function()
+    {
+        gBrowser.selectedTab = gBrowser.addTab("http://www.shangducms.com/");
+    },
+
+    StartDrag:function(event,type)
+    {
+        if(event.button!=1)
+        {
+            //alert('start');
+            var dx = 0;
+            var dy = 0;
+            //check if the first time drag
+            //this method will be called lots of time
+            if(this.times!=0)
+            {
+                dx = event.clientX - this.x;
+                dy = event.clientY - this.y;
+                if(dx>0)
+                {
+                    //open page in new tab
+                    this.method="new";
+                }
+                else if(dx<0)
+                {
+                    //open page in this tab
+                    this.method="this";
+                }
+            }
+            else
+            {
+                this.x = event.clientX;
+                this.y = event.clientY;
+            }
+
+            this.times++;
+        }
+        else
+        {
+            if(type=='down')
+            {
+                this.rx = event.clientX;
+                this.ry = event.clientY;
+            }
+            else
+            {
+                if(event.clientX-this.rx<0)
+                {
+                    getBrowser().goBack();
+                }
+            }
+        }
+    },
+
+    DragOver:function(event)
+    {
+
+    },
+
+    DragDrop:function(event)
+    {
+
+    }
+};
+
+addEventListener("load", DragUrLink.OnMainLoad , false);
---
+++ src/content/wt.ff.Pref.js
@@ -0,0 +1,135 @@
+if(typeof(___)=="undefined"||!___) ___ ={};
+if(!___.PrefImpl) ___.PrefImpl = function(){};
+
+___.PrefImpl.prototype  =
+{
+    _branchName:"",
+	_pref : Components.classes["@mozilla.org/preferences-service;1"].
+	getService(Components.interfaces.nsIPrefService).
+	getBranch("extensions.jguo.{appname}."),
+
+	getValue: function(prefName, defaultValue)
+	{
+		var prefType=this._pref.getPrefType(prefName);
+
+		if (prefType==this._pref.PREF_INVALID)
+		{
+			if(defaultValue)
+			{
+				return defaultValue;
+			}
+			else
+			{
+				return "";
+			}
+		}
+
+		switch (prefType)
+		{
+			case this._pref.PREF_STRING: return this._pref.getCharPref(prefName);
+			case this._pref.PREF_BOOL: return this._pref.getBoolPref(prefName);
+			case this._pref.PREF_INT: return this._pref.getIntPref(prefName);
+		}
+	},
+
+	setValue: function(prefName, value)
+	{
+		var prefType=typeof(value);
+
+		switch (prefType)
+		{
+            case "string":
+            case "boolean":
+                break;
+            case "number":
+                if (value % 1 != 0)
+                {
+                    throw new Error("Cannot set preference to non integral number");
+                }
+                break;
+            default:
+                throw new Error("Cannot set preference with datatype: " + prefType);
+		}
+
+		if (this.exists(prefName) && prefType != typeof(this.getValue(prefName)))
+		{
+			this.remove(prefName);
+		}
+
+		// set new value using correct method
+		switch (prefType)
+		{
+			case "string": this._pref.setCharPref(prefName, value); break;
+			case "boolean": this._pref.setBoolPref(prefName, value); break;
+			case "number": this._pref.setIntPref(prefName, Math.floor(value)); break;
+		}
+	},
+	
+	setBoolValue: function(prefName, value)
+	{
+		var prefType=typeof(value);
+		
+		if (this.exists(prefName) && prefType != typeof(this.getValue(prefName)))
+		{
+			this.remove(prefName);
+		}
+		
+		if(!value)
+		{
+			value = false;
+		}
+		this._pref.setBoolPref(prefName, value);
+	},
+	
+	setIntValue: function(prefName, value)
+	{
+		var prefType=typeof(value);
+		
+		if (this.exists(prefName) && prefType != typeof(this.getValue(prefName)))
+		{
+			this.remove(prefName);
+		}
+		
+		if(!value)
+		{
+			value = 0;
+		}
+		
+		this._pref.setIntPref(prefName, Math.floor(value));
+	},
+	
+	getBoolValue: function(prefName, value)
+	{
+		return this.getValue(prefName);
+	},
+	
+	getIntValue: function(prefName, value)
+	{
+		return this.getValue(prefName);
+	},
+	
+	getDefaultBranch: function()
+	{
+		this._pref.getDefaultBranch();
+	},
+
+	remove: function(prefName)
+	{
+		this._pref.deleteBranch(prefName);
+	},
+	
+	deleteBranch: function()
+	{
+		var children = this._pref.getChildList("", {});
+		var prefNum=0;
+		for(prefNum=0;prefNum<children.length;prefNum++)
+		{
+			this.remove(children[prefNum]);
+		}
+	},
+
+	exists: function(prefName)
+	{
+		return this._pref.getPrefType(prefName) != 0;
+	}
+};
